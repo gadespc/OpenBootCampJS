5faeb53efe471db9dfb38efc99e4ac44f216d4c2
gadespc <gadespc@users.noreply.github.com>
2022-05-02 03:17:19 +0200

    Add files via upload

---
+++ ejercicioTema1/proyecto1node/index.js
@@ -0,0 +1,8 @@
+// Muestra texto y comprueba si es Hola picha para imprimir otro mensaje
+
+const newText = "Estoy realizando el curso de JavaScript, espero aprender mucho";
+console.log(newText);
+
+if (newText == "Estoy realizando el curso de JavaScript, espero aprender mucho") {
+  console.log("Adiós picha!");
+}
