1a088a4d1567d8ccbc82ee7f235d19880ab44114
gadespc <gadespc@users.noreply.github.com>
2022-05-05 20:15:42 +0200

    Add files via upload

---
+++ ejercicioTema12/tema/index.js
@@ -0,0 +1,18 @@
+let a = 10;
+let b = 20;
+let c = "Hola";
+
+let misuma = a + b + c;
+console.log(misuma);
+
+for (let i = 0; i < 10; i++) {
+    if(i == 5) break
+}
+
+const f = function(p1, p2, p3) {
+    let divide = p1 / p2;
+    return p3 + p1 + p2
+}
+
+let resultado = f(a, b, c);
+console.log(resultado);
